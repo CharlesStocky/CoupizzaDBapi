3ee0f958daf92309d4b81a91cb8df682bb73f9a3
Charles Stocky <charlesstocky@Charless-MacBook-Pro.local>
2018-03-06 21:25:13 -0600

    refactored/updated facebook account finder

---
+++ FBDataGen/accountFinder/accountGetter.js
@@ -0,0 +1,25 @@
+import graph from 'fbgraph'; 
+import { MongoClient } from 'mongodb';  
+
+const endpoint = '/34703237638/locations?fields=location&after=' //this api endpoint retrieves all locations of a specified business   
+
+graph.setAccessToken(process.env.fbAT)
+
+export default () => {
+  return new Promise((resolve, reject)=>{
+    let dataArr = []
+    graph.get(endpoint, function dataParse(err, res){
+      if(err) return console.log(err) 
+      res.data.filter((obj)=>{
+        if(obj.location.country === 'United States'){
+          dataArr.push(obj)
+        }
+      })
+      if(res.paging && res.paging.next){
+        graph.get(res.paging.next, dataParse);
+      }else{ 
+        resolve(dataArr)
+      }
+    })
+  })
+}
--- FBDataGen/accountFinder/app.js
+++ FBDataGen/accountFinder/app.js
@@ -1,14 +1,23 @@
 import graph from 'fbgraph'; 
-import { MongoClient } from 'mongodb'; 
+import { MongoClient } from 'mongodb';  
 
+import accountGetter from './accountGetter.js'
 
-graph.setAccessToken(process.env.fbAT) 
+(async () => {
+  const fbObjArray = await accountGetter()
 
-let cursor = 'MjAwMDIwMjM2NjkzMTA5'  
-
-graph.get('/34703237638/locations?fields=location&after=', (err, res)=>{
-  if(err) console.log(err); 
-  res.data.filter((locale)=>{
-    console.log(locale) 
+  return await MongoClient.connect('mongodb://localhost:27017', (err, client)=>{
+    const db = client.db('papa')
+    const collection = db.collection('FBAccounts')
+    fbObjArray.forEach((obj)=>{
+      collection.find({"ID": obj.id}).toArray((err, docs)=> {
+        if(err) return console.log(err)
+        if(docs.length === 0){
+          collection.insert({"ID": obj.id, "zip": obj.location.zip, "city": obj.location.city, "state": obj.location.state, "lat": obj.location.latitude, "long": obj.location.longitude}); 
+        }
+      })
+    })
+    console.log('completed')
   })
-})
+})()
+

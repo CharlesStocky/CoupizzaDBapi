42226814b5556f122fcec8fb37c17d9a3c53539e
Charles Stocky <charlesstocky@Charless-MacBook-Pro.local>
2018-03-24 13:23:33 -0500

    replacing FBUAT

--- FBDataGen/codeFinder/postGetter.js
+++ FBDataGen/codeFinder/postGetter.js
@@ -2,7 +2,7 @@ import graph from 'fbgraph'
 import dateFormatter from './dateFormatter.js'
 import _ from 'underscore'
 
-graph.setAccessToken('EAACEdEose0cBALDlwd1KxulTrxZBe1ph3rYCWmWgfvhSyKKnG5o40ADPgKeHXSbvQtlasZCFSF6ZAJqqE9TkXGlT8cZB0QnelCv6T8ZAI3Ssmg86et8gRDFwXh7FhvlIyhjt7BnYJfRwbIjBmrc64nUk4lie5h1w1pfaZCRCW4lMCE9KVSJs71G6Xl8sI4RDH9Eedv2MyYcgXxG8Xr8BIW')
+graph.setAccessToken(process.env.FB_UAT)
 
 const dateStr = dateFormatter()
 
@@ -26,7 +26,7 @@ export default (FBAccountObj) => {
       })
     })
     if(counter === FBAccountObj.length){
-      console.log('Account ID list exhausted')
+      console.log('ID list exhausted') 
       resolve(postArr)
     }
   })
